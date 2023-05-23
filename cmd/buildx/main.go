b3f6ef0f546f1cd8fd3a7a93b75272772452b542
Mmx233 <admin@mmxblog.com>
2023-05-23 20:59:06 +0800

    feat: 执行构建流程

--- cmd/buildx/main.go
+++ cmd/buildx/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"github.com/MultiMx/GoBuildxAction/internal/global"
+	"github.com/MultiMx/GoBuildxAction/internal/pkg/goBuild"
+	"strings"
+)
 
+func main() {
+	arches := strings.Split(global.Config.Platform, ",")
+	builder := goBuild.NewMultiPlatformBuilder(
+		global.Config.Name,
+		global.Config.Args,
+		global.Config.Target, arches,
+	)
+	builder.Build()
 }
--- internal/global/models/config.go
+++ internal/global/models/config.go
@@ -1,6 +1,8 @@
 package models
 
 type Config struct {
-	Name string `env:"NAME,required"`
-	Args string `env:"ARGS"`
+	Name     string `env:"NAME,required"`
+	Args     string `env:"ARGS"`
+	Platform string `env:"PLATFORM,required"`
+	Target   string `env:"TARGET"`
 }
