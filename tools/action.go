0218ee6ba107bce7f8dd917c13dd4ecb409e7229
Mmx233 <admin@mmxblog.com>
2023-05-23 22:32:15 +0800

    fix: 尝试修复输出异常

--- tools/action.go
+++ tools/action.go
@@ -8,7 +8,7 @@ import (
 
 func SetOutput(name, value string) {
 	value = strings.ReplaceAll(value, "\"", `\"`)
-	e := exec.Command("bash", "-c", fmt.Sprintf("echo \"%s=%s\" >> $GITHUB_OUTPUT", name, value)).Run()
+	e := exec.Command("/bin/sh", "-c", fmt.Sprintf("echo \"%s=%s\" >> $GITHUB_OUTPUT", name, value)).Run()
 	if e != nil {
 		panic(e)
 	}
