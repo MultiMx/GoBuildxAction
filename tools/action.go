9a9f104040cf48bd02b0ff0807233654f036a1c0
Mmx233 <admin@mmxblog.com>
2023-05-23 23:21:28 +0800

    fix: 去除多余的 output 值转义

--- tools/action.go
+++ tools/action.go
@@ -3,11 +3,9 @@ package tools
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 func SetOutput(name, value string) {
-	value = strings.ReplaceAll(value, "\"", `\"`)
 	file, e := os.OpenFile(os.Getenv("GITHUB_OUTPUT"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
 	if e != nil {
 		panic(e)
